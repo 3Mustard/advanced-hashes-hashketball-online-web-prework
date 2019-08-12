b0a9c3b9866bb1773d1b480f4629a38f055856f3
Matthew C <m23.cullen@gmail.com>
2019-08-12 21:14:07 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -169,7 +169,7 @@ def player_stats(player_n)
   game_hash.each do |home_away,keys|
     keys[:players] do |player|
       if player[:player_name] == player_n
-        return player
+        
       end
     end
   end
