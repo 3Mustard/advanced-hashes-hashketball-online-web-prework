47283b0afc2f031f0f72235e3c1486fc3bdc3e6f
Matthew C <m23.cullen@gmail.com>
2019-08-12 19:50:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,7 +3,10 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black","White"],
-      :players => {}
+      :players => {
+        
+        
+      }
     }
     :away => {
       :team_name => "",
